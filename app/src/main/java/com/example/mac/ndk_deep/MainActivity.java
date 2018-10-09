27b679743e60e0b3680f93729e4fbbff7b622f98
mac <1>
2018-10-09 10:15:27 +0800

    增加文档

--- app/src/main/java/com/example/mac/ndk_deep/MainActivity.java
+++ app/src/main/java/com/example/mac/ndk_deep/MainActivity.java
@@ -25,5 +25,5 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public  static native String stringFromJNI();
 }
